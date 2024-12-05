3be19f8f72e9c9ccbc8639aff41e11f03d03d1fe
Electrifier <samchijioke16@gmail.com>
2024-12-05 12:15:55 +0100

    Fix: Passed the `ExtendedContext` type to `Telegraf` bot type parameter

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,10 +1,15 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Telegram } from "telegraf";
 
 import APP_SETTINGS from "../config";
 import { StartController } from "../controllers";
+import { UserMiddleware } from "../middleware";
+import { ExtendedContext } from "../types";
+
 
 const setupBot = async () => {
-    const bot = new Telegraf(APP_SETTINGS.TG_TOKEN);
+    const bot = new Telegraf<ExtendedContext>(APP_SETTINGS.TG_TOKEN);
+
+    bot.use(UserMiddleware.addUserToContext);
 
     bot.start(StartController.processStart);
 
