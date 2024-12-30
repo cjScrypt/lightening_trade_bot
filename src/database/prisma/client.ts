67259faecfde58967703d7effa1f418ae5afa59c
Electrifier <samchijioke16@gmail.com>
2024-12-30 19:32:03 +0100

    Made prisma datasource declaration to evaluate at runtime

--- src/database/prisma/client.ts
+++ src/database/prisma/client.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+export default new PrismaClient(
+	{
+		datasources: { // @note Use this to evaluate value at runtime
+			db: { url: process.env.DB_URL }
+		}
+	}
+);
