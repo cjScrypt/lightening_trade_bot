9ea82313c8bd9f3da702c5605b676346065eb8e3
Electrifier <samchijioke16@gmail.com>
2025-01-03 06:58:30 +0100

    Added 'scene' field to ExtendedContext type

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,7 +1,8 @@
-import { Context } from "telegraf";
+import { Context, Scenes } from "telegraf";
 import { User } from "telegraf/typings/core/types/typegram";
 
 
 export interface ExtendedContext extends Context {
     user: User;
+    scene: Scenes.SceneContextScene<ExtendedContext>;
 }
