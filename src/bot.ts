bcf305066a0b1de730f760d3f5f751311be8e69c
Electrifier <samchijioke16@gmail.com>
2025-01-03 07:19:03 +0100

    Add scene Stage to bot

--- src/bot.ts
+++ src/bot.ts
@@ -4,10 +4,13 @@ import APP_SETTINGS from "./config";
 import { StartController } from "./controllers";
 import { UserMiddleware } from "./middleware";
 import { ExtendedContext } from "./types";
+import { mainStage } from "./scenes";
 
 
 export const setupBot = () => {
     const bot = new Telegraf<ExtendedContext>(APP_SETTINGS.TG_TOKEN);
+
+    bot.use(mainStage.middleware());
 
     bot.use(UserMiddleware.addUserToContext);
 
--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,1 +1,9 @@
-export * from "./wallet";
+import { Scenes } from "telegraf";
+
+import { ExtendedContext } from "../types";
+import { walletScene } from "./wallet";
+
+
+export const mainStage = new Scenes.Stage<ExtendedContext>(
+    [ walletScene ]
+)
