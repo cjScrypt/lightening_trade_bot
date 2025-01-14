960b8c6b79d96cabacf370467ecaf5580b239757
Electrifier <samchijioke16@gmail.com>
2025-01-14 16:10:14 +0100

    Update TelegramUtils.getUserFromContext method to throw error on missing message originator

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -2,8 +2,15 @@ import { ExtendedContext } from "../types";
 import { Chat, User } from "telegraf/typings/core/types/typegram";
 
 export class TelegramUtils {
-    static getUserFromContext(ctx: ExtendedContext): User | undefined {
-        return ctx.callbackQuery?.from || ctx.from;
+    static getUserFromContext(ctx: ExtendedContext): User {
+        const user = ctx.callbackQuery?.from || ctx.from;
+        if (!user) {
+            throw new Error(
+                "Current Context is missing the message originator property."
+            );
+        }
+
+        return user;
     }
 
     static isPrivateChat(chat: Chat | undefined): boolean {
