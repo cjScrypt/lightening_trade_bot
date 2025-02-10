14019154530e59c414775d103ee2f634bdefc7bc
Electrifier <samchijioke16@gmail.com>
2025-02-10 04:31:53 +0100

    Implement buyJetton method

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,6 @@ export default {
     TG_TOKEN: process.env.TG_TOKEN || "",
 	WEBHOOK_DOMAIN: process.env.WEBHOOK_DOMAIN || "",
     TON_API_KEY: process.env.TON_API_KEY || "",
-    TON_API_ENDPOINT: process.env.TON_API_ENDPOINT || ""
+    TON_API_ENDPOINT: process.env.TON_API_ENDPOINT || "",
+    ROUTER_ADDRESS: process.env.ROUTER_ADDRESS || ""
 }
--- src/services/stonfi.service.ts
+++ src/services/stonfi.service.ts
@@ -1,13 +1,38 @@
-import { OpenedContract, TonClient } from "@ton/ton";
-
 import { DEX, pTON } from "@ston-fi/sdk";
 import { RouterV1 } from "@ston-fi/sdk/dist/contracts/dex/v1/RouterV1";
+import { PtonV1 } from "@ston-fi/sdk/dist/contracts/pTON/v1/PtonV1";
+import { OpenedContract, toNano, TonClient } from "@ton/ton";
+
+import { TonApiService } from "./";
+import APP_SETTINGS from "../config";
 
 
 export class StonFiService {
-    dex: OpenedContract<RouterV1>;
+    router: OpenedContract<RouterV1>;
+    proxyTon: PtonV1;
 
     constructor(client: TonClient) {
-        this.dex = client.open(new DEX.v1.Router());
+        this.router = client.open(new DEX.v1.Router(APP_SETTINGS.ROUTER_ADDRESS));
+        this.proxyTon = new pTON.v1();
+    }
+
+    async buyJetton(data: {
+        privateKey: string,
+        publicKey: string,
+        amount: bigint,
+    }) {
+        const tonService = new TonApiService();
+        const txParams = await this.router.getSwapTonToJettonTxParams({
+            userWalletAddress: "",
+            proxyTon,
+            offerAmount: toNano(data.amount),
+            askJettonAddress: jettonContractAddress,
+            minAskAmount: "",
+            queryId: 0
+        });
+        await  tonService.sendTransaction(
+            txParams,
+            { privateKey: data.privateKey, publicKey: data.publicKey }
+        );
     }
 }
