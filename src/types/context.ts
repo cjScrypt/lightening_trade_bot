d05b84e88d4fdb1396f692ffed6c5635d7b734d0
Electrifier <samchijioke16@gmail.com>
2025-02-14 02:53:20 +0100

    Add 'RedisSession' to 'ExtendedContext' for storing cached data

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,9 +1,11 @@
 import { Context, Scenes } from "telegraf";
-
 import { User } from "@prisma/client";
+
+import { RedisSession } from "./redisSession";
 
 
 export interface ExtendedContext extends Context {
+    redisSession: RedisSession;
     user: User;
     scene: Scenes.SceneContextScene<ExtendedContext>;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./context";
+export * from "./redisSession";
 export * from "./wallet";
 export * from "./user";
